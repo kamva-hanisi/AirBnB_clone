885749b5d65ecd9949c55aea0a4df3e2314dea21
kamva-hanisi <lucashanisi@gmail.com>
2023-11-14 10:01:41 -0800

    Adding unit tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,16 +1,59 @@
 #!/usr/bin/python3
+"""Defines unittests for models/base_model.py."""
+import unittest
 from models.base_model import BaseModel
+from datetime import datetime
+from models import storage
 
-my_model = BaseModel()
-my_model.name = "My First Model"
-my_model.my_number = 89
-print(my_model)
-my_model.save()
-print(my_model)
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key,
-                                   type(my_model_json[key]),
-                                   my_model_json[key]))
+
+class TestBaseModel(unittest.TestCase):
+
+    def setUp(self):
+        self.base_model_instance = BaseModel()
+
+    def tearDown(self):
+        storage._FileStorage__objects = {}
+
+    def test_instance_creation(self):
+        self.assertIsNotNone(self.base_model_instance.id)
+        self.assertIsInstance(self.base_model_instance.created_at, datetime)
+        self.assertIsInstance(self.base_model_instance.updated_at, datetime)
+
+    def test_str_representation(self):
+        expected_str = "[BaseModel] ({}) {}".format(
+            self.base_model_instance.id, self.base_model_instance.__dict__)
+        self.assertEqual(str(self.base_model_instance), expected_str)
+
+    def test_save_method(self):
+        initial_updated_at = self.base_model_instance.updated_at
+        self.base_model_instance.save()
+        self.assertNotEqual(initial_updated_at,
+                            self.base_model_instance.updated_at)
+
+    def test_to_dict_method(self):
+        expected_dict = {
+            'id': self.base_model_instance.id,
+            '__class__': 'BaseModel',
+            'created_at': self.base_model_instance.created_at.isoformat(),
+            'updated_at': self.base_model_instance.updated_at.isoformat()
+        }
+        self.assertEqual(self.base_model_instance.to_dict(), expected_dict)
+
+    def test_kwargs_initialization(self):
+        kwargs = {
+            'id': 'test-id',
+            'created_at': '2023-01-01T12:00:00.000000',
+            'updated_at': '2023-01-02T12:00:00.000000',
+            'custom_attr': 'test_value'
+        }
+        base_model_instance = BaseModel(**kwargs)
+        self.assertEqual(base_model_instance.id, kwargs['id'])
+        self.assertEqual(base_model_instance.custom_attr, 'test_value')
+
+    def test_invalid_args_initialization(self):
+        with self.assertRaises(TypeError):
+            BaseModel('invalid_arg')
+
+
+if __name__ == '__main__':
+    unittest.main()
